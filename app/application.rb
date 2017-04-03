02ac6907e6bc7e3d263ad16ca387d0b472d97720
ethanryan <ethanryan@gmail.com>
2017-04-03 13:03:44 -0400

    done

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,12 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
 
+#Create a new class array called @@cart to hold any items in your cart
+  @@cart = []
+
   def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    resp = Rack::Response.new #server responding
+    req = Rack::Request.new(env) #brower requesting content from the server
 
     if req.path.match(/items/)
       @@items.each do |item|
@@ -14,11 +17,48 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
-      resp.write "Path Not Found"
+      resp.write "Your cart is empty"
     end
 
-    resp.finish
+#Create a new route called /cart to show the items in your cart
+    if req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end #end loop
+
+      #Create a new route called /add that takes in a GET param
+      #with the key item. This should check to see if that item
+      #is in @@items and then add it to the cart if it is.
+      #Otherwise give an error
+
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      resp.write handle_search(search_term)
+
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
+
+    # elsif req.path.match(/add/)
+    #   search_term = req.params["q"]
+    #   resp.write handle_search(search_term)
+    #     #resp.write "#{item}\n"
+    #   @@cart << search_term
+    #   resp.write "added #{search_term}\n"
+
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    else
+      resp.write "We don't have that item"
+    end #end if..elsif..else statement
+
+    resp.finish #this ends call method.
   end
+
 
   def handle_search(search_term)
     if @@items.include?(search_term)
@@ -27,4 +67,4 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
-end
+end #end class
